61d3185af79dde1c0370fe3b47f4ce445c122dc0
Adam <70108536+fanchea84@users.noreply.github.com>
2021-01-17 12:29:55 -0800

    plotly challenge

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,12 @@
-// STRATEGY
-// Intialize Dashboard page
-// 1. Establish default Test Subject ID 940 <== write a function for this
-// 2. Populdate dropdown with possible choices (Test Subject ID Numbers)
-// {/* <option value="hamster">Hamster</option>     <== hint for list of options in dropdown */}
-// Write 2 functions to carry out responsibilities: one for establishing defaults (item 1 above), one for responding to user input 
-
-
 // Create DEMOGRAPHICDATA function to get the metadata for given patient ID
 function DemographicData() {
-    d3.json("samples.json").then((data) => {
-        var metadata = data.metadata;
-        console.log(metadata);
+    d3.json("samples.json").then(function(data) {
+        // 
+        console.log(data.metadata);
     })
 }
+// Call DEMOGRAPHICDATA function
+DemographicData();
 
 // Create INIT function to populate dropdown menu with patient IDs (Patient IDs taken from "names" column in samples.json)
 function init() {
@@ -25,7 +19,13 @@ function init() {
         })
     });
 }
-
 // Call INIT function
-init();
+init(); 
+
 // https://stackoverflow.com/questions/43121679/how-to-append-option-into-select-combo-box-in-d3
+// STRATEGY
+// Intialize Dashboard page
+// 1. Establish default Test Subject ID 940 <== write a function for this
+// 2. Populdate dropdown with possible choices (Test Subject ID Numbers)
+// {/* <option value="hamster">Hamster</option>     <== hint for list of options in dropdown */}
+// Write 2 functions to carry out responsibilities: one for establishing defaults (item 1 above), one for responding to user input 
